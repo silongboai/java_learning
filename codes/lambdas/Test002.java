ed49dfa5d26a181300c82529f8df10c2415406ae
silongboai <silongboai@gmail.com>
2024-02-20 18:32:49 -0500

    $

--- codes/lambdas/Test002.java
+++ codes/lambdas/Test002.java
@@ -1,8 +1,19 @@
 package codes.lambdas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.function.IntSupplier;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.IntPredicate;
 
 public class Test002 {
     public static void main(String[] args) {
@@ -23,5 +34,63 @@ public class Test002 {
         for (int i = 0; i < 5; i++) {
             System.out.println(supplier3.getAsInt());
         }
+
+        //
+        Consumer<Integer> printer = i -> System.out.println(i);
+        for (int i = 0; i < 5; i++) {
+            printer.accept(supplier3.getAsInt());
+        }
+        //  IntConsumer, LongConsumer, and DoubleConsumer
+        IntConsumer consumer = i -> System.out.println(i);
+        for (int i = 0; i < 5; i++) {
+            consumer.accept(supplier3.getAsInt());
+        }
+
+        // Consuming Two Elements with a BiConsumer
+        // ObjIntConsumer<T>, ObjLongConsumer<T> and ObjDoubleConsumer<T>
+        BiConsumer<Random, Integer> randomNumberConsumer =
+            (rand, num) -> {
+                for (int i = 0; i < num; i++) {
+                    System.out.println(rand.nextInt(10));
+                }
+            };
+        randomNumberConsumer.accept(random, 5);
+
+        // Passing a Consumer to an Iterable
+        Consumer<String> c = s -> System.out.println(s);
+        List<String> strings = List.of("Hello", "world");
+        strings.forEach(c);
+
+        //
+        Predicate<String> length3 = s -> s.length() == 3;
+        System.out.println(length3.test("hello"));
+
+        // IntPredicate, LongPredicate, and DoublePredicate
+        IntPredicate isGreaterThan10 = i -> i > 10;
+        System.out.println(isGreaterThan10.test(11));
+
+        // Testing Two Elements with a BiPredicate
+        BiPredicate<String, Integer> isOfLength = (word, length) -> word.length() == length;
+        System.out.println(isOfLength.test("hello", 3));
+
+        // Passing a Predicate to a Collection
+        List<String> immutableStrings = List.of("one", "two", "three", "four", "five");
+        List<String> strings1 = new ArrayList<>(immutableStrings);
+        Predicate<String> isOddLength = s -> s.length() % 2 == 0;
+        strings1.removeIf(isOddLength);
+        System.out.println(strings1);
+
+        //
+        Function<String, Integer> toLength = s -> s.length();
+        String word = "test";
+        int length = toLength.apply(word);
+        System.out.println(length);
+
+        // 16 specialized types of functions
+        // Passing a Unary Operator to a List
+        List<String> strings2 = Arrays.asList("one", "two", "three");
+        UnaryOperator<String> toUpperCase = w -> w.toUpperCase();
+        strings2.replaceAll(toUpperCase);
+        System.out.println(strings2);
     }
 }
--- docs/lambdas.md
+++ docs/lambdas.md
@@ -2,19 +2,18 @@
 - 使用Lambda表达式提高代码的可读性
 - 从 Java 8 版本开始加入, 2014年
 
-## 1. 尝试使用Lambda表达式
+## 1. 认识Lambda表达式
 - 写匿名类实例的简便方法
 - 三步: 确定lambda表达式的类型(functional interface), 找出要实现的方法,  使用lambda表达式来实现该方法.
 - syntax: ()->{}
 - lambdas只能捕获值, 而不能捕获变量; 在lambda表达式中使用的变量, 必须是final或者effectively final(Java SE 8)
 
-## 2. Using Lambdas Expressions in Your Application
+## 2. 使用Lambdas表达式
 - a new package: java.util.function, in the java.base module.
-- Supplier<T>: 没有参数, 返回一个对象
+- Supplier<T>: 没有参数, 返回一个值
 - Consumer<T>: 接收一个参数, 不返回任何东西
-
-
-
+- Predicate<T>: 接收一个参数, 返回一个布尔值
+- Function<T, R>: 接收一个参数, 返回另一个类型的值
 
 ## 3. Writing Lambda Expressions as Method References
 
